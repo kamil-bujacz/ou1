d0a52356bfcf571c3fe788c366b9f8dfb8313d05
Kamil Bujacz <kiklas@MacBook-Air-Kamil.local>
2022-01-13 16:31:52 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ou1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ou1.dir/main.c.o"
+  "ou1"
+  "ou1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ou1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ou1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ou1.
+# This may be replaced when dependencies are built.
